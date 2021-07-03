5c63b4d5ee19bb91490f26e0acb866e33ca3bea7
Anton Ohorodnyk <anton@ohorodnyk.name>
2021-07-02 17:55:35 -0700

    #3 Add example for ParseAcceptHeader

--- parser_accept_test.go
+++ parser_accept_test.go
@@ -1,11 +1,32 @@
 package mimeheader_test
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/aohorodnyk/mimeheader"
 )
+
+func ExampleParseAcceptHeader() {
+	header := "image/*; q=0.9; s=4, application/json; q=0.9; b=3;, text/plain,image/png;q=0.9, image/jpeg,image/svg;q=0.8"
+	ah := mimeheader.ParseAcceptHeader(header)
+
+	fmt.Println(ah.Negotiate([]string{"application/xml", "image/tiff"}, "text/javascript"))
+	fmt.Println(ah.Negotiate([]string{"application/xml", "image/png"}, "text/javascript"))
+	fmt.Println(ah.Negotiate([]string{"application/xml", "image/svg"}, "text/javascript"))
+	fmt.Println(ah.Negotiate([]string{"text/dart", "application/dart"}, "text/javascript"))
+
+	fmt.Println(ah.Match("image/svg"))
+	fmt.Println(ah.Match("text/javascript"))
+	// Output:
+	// image/* image/tiff true
+	// image/png image/png true
+	// image/* image/svg true
+	//  text/javascript false
+	// true
+	// false
+}
 
 func TestParseAcceptHeader(t *testing.T) {
 	t.Parallel()
